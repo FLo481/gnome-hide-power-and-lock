97dda36fdf8871be6ae64ce95492903d9ab7c988
Florian <pennerassinger@gmail.com>
2023-12-01 16:51:22 +0100

    Beautifications and removed unused variable

--- extension.js
+++ extension.js
@@ -1,28 +1,23 @@
 const Main = imports.ui.main;
-const St = imports.gi.St;
 
 let orgIndicator, Menu;
 
 class Extension {
     constructor() {
-	    this._systemMenu = Main.panel.statusArea.aggregateMenu._system;
+	this._systemMenu = Main.panel.statusArea.aggregateMenu._system;
         orgIndicator = Main.panel.statusArea.aggregateMenu._power;
         Menu = Main.panel.statusArea.aggregateMenu._system.menu;
     }
 
     enable() {
         this._systemMenu._lockScreenItem.visible = false;
-
         orgIndicator.indicators.hide();
-
         Menu.actor.remove_child(Main.panel.statusArea.aggregateMenu._system._sessionSubMenu);  
     }
 
     disable() {
         this._systemMenu._lockScreenItem.visible = true;
-
         orgIndicator.indicators.show();
-
         Menu.box.insert_child_at_index(Main.panel.statusArea.aggregateMenu._system._sessionSubMenu, Main.panel.statusArea.aggregateMenu._system.menu.numMenuItems)
     }
 }
